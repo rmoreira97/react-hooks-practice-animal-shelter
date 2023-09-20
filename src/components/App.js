5ce1b7bd3a495b64d16bca6ba20fc28384f0cc60
Rafael Moreira <rafa.siffert@gmail.com>
2023-09-19 20:15:07 -0400

    finished

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,31 @@
-import React, { useState } from "react";
-
+import React, { useState, useEffect } from "react";
 import Filters from "./Filters";
 import PetBrowser from "./PetBrowser";
 
 function App() {
   const [pets, setPets] = useState([]);
   const [filters, setFilters] = useState({ type: "all" });
+
+  useEffect(() => {
+    fetchPets();  // Fetch all pets by default when component mounts
+  }, []);  // Empty dependency array to ensure it runs only once after mount
+
+  const fetchPets = () => {
+    let endpoint = 'http://localhost:3001/pets';
+    if (filters.type !== 'all') {
+      endpoint += `?type=${filters.type}`;
+    }
+    fetch(endpoint)
+      .then(res => res.json())
+      .then(data => setPets(data));
+  }
+
+  const handleAdoptPet = (id) => {
+    const updatedPets = pets.map(pet => 
+      pet.id === id ? {...pet, isAdopted: true} : pet
+    );
+    setPets(updatedPets);
+  }
 
   return (
     <div className="ui container">
@@ -15,10 +35,10 @@ function App() {
       <div className="ui container">
         <div className="ui grid">
           <div className="four wide column">
-            <Filters />
+            <Filters onChangeType={setFilters} onFindPetsClick={fetchPets} />
           </div>
           <div className="twelve wide column">
-            <PetBrowser />
+            <PetBrowser pets={pets} onAdoptPet={handleAdoptPet} />
           </div>
         </div>
       </div>
